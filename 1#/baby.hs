24211f18bd6698a090e99801ccfdca0e72cc7886
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-03-10 18:13:06 -0300

    finishing first chapter

--- 1#/baby.hs
+++ 1#/baby.hs
@@ -6,6 +6,6 @@ doubleSmallNumber x = if x > 100
                         then x
                         else doubleMe x
 
-doubleSmallNumber' x ( if x > 100 then x else doubleMe x) 
+doubleSmallNumber' x = ( if x > 100 then x else doubleMe x) 
 
 
